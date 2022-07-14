54a291f2a116eacd4515d09c779321864c581057
Amanuel Odicho <53154523+odicho@users.noreply.github.com>
2022-07-14 00:40:49 -0500

    added first chunk of html/css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-	return <div className="">Hello World</div>;
+	return (
+		<div className="flex h-screen w-screen flex-col items-center justify-center">
+			<div className="text-center text-2xl">Which Pokemon is rounder?</div>
+			<div className="p-2" />
+			<div className="flex max-w-2xl items-center justify-between rounded border p-8">
+				<div className="h-16 w-16 bg-red-200" />
+				<div className="p-8">VS</div>
+				<div className="h-16 w-16 bg-red-200" />
+			</div>
+		</div>
+	);
 }
