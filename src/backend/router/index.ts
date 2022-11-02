37905998526c6294dbea06ce8c24a5f78ee5860d
Amanuel Odicho <53154523+odicho@users.noreply.github.com>
2022-11-01 22:02:43 -0700

    Added prisma and one endpoint for voting

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -2,6 +2,7 @@ import { initTRPC } from '@trpc/server';
 import { z } from 'zod';
 
 import { PokemonClient } from 'pokenode-ts';
+import { prisma } from "@/backend/utils/prisma"
 
 export const t = initTRPC.create();
 
@@ -17,7 +18,31 @@ export const appRouter = t.router({
             const pokemon = await api.getPokemonById(input.id);
             return { name: pokemon.name, sprites: pokemon.sprites };
         }),
+    "cast-vote": t.procedure
+        .input(
+            z.object({
+                votedFor: z.number(),
+                votedAgainst: z.number()
+            }),
+        ).mutation(async ({ input }) => {
+            const voteInDb = await prisma.vote.create({
+                data: {
+                    ...input
+                }
+            })
+            return { success: true, vote: voteInDb }
+        })
 });
+
+// .mutation("cast-vote", {
+//     input: z.object({
+//         votedFor: z.number(),
+//         votedAgainst: z.number()
+//     }),
+//     async resolve() {
+//         return {success: true}
+//     }
+
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
