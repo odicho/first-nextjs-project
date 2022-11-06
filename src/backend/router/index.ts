e126fe2e57c4d0a6972fa91c32fdef435f8a842f
Amanuel Odicho <53154523+odicho@users.noreply.github.com>
2022-11-06 01:06:50 -0700

    dependency issue fix

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -1,39 +1,40 @@
 import { initTRPC } from "@trpc/server";
 import { z } from "zod";
 
-import { prisma } from "@/backend/utils/prisma"
+import { prisma } from "@/backend/utils/prisma";
 
 export const t = initTRPC.create();
 
 export const appRouter = t.router({
-    "get-pokemon-by-id": t.procedure
-        .input(
-            z.object({
-                id: z.number()
-            })
-        )
-        .query(async ({ input }) => {
+	"get-pokemon-by-id": t.procedure
+		.input(
+			z.object({
+				id: z.number(),
+			})
+		)
+		.query(async ({ input }) => {
 			const pokemon = await prisma.pokemon.findFirst({
 				where: { id: input.id },
 			});
 			if (!pokemon) throw new Error("Pokemon doesn't exist with this id");
 			return pokemon;
-        }),
-    "cast-vote": t.procedure
-        .input(
-            z.object({
-                votedFor: z.number(),
-                votedAgainst: z.number()
-            }),
-        ).mutation(async ({ input }) => {
-            const voteInDb = await prisma.vote.create({
-                data: {
-                    votedForId: input.votedFor,
+		}),
+	"cast-vote": t.procedure
+		.input(
+			z.object({
+				votedFor: z.number(),
+				votedAgainst: z.number(),
+			})
+		)
+		.mutation(async ({ input }) => {
+			const voteInDb = await prisma.vote.create({
+				data: {
+					votedForId: input.votedFor,
 					votedAgainstId: input.votedAgainst,
-                }
-            })
-            return { success: true, vote: voteInDb }
-        })
+				},
+			});
+			return { success: true, vote: voteInDb };
+		}),
 });
 
 // .mutation("cast-vote", {
@@ -44,7 +45,6 @@ export const appRouter = t.router({
 //     async resolve() {
 //         return {success: true}
 //     }
-
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
