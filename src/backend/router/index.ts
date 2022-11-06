409fecfaf558e368fbb53c309a94eb00245492d6
Amanuel Odicho <53154523+odicho@users.noreply.github.com>
2022-11-06 00:39:29 -0700

    migrated to using internal db instead of api calls

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -1,37 +1,41 @@
-import { initTRPC } from '@trpc/server';
-import { z } from 'zod';
+import { initTRPC } from "@trpc/server";
+import { z } from "zod";
 
-import { PokemonClient } from 'pokenode-ts';
-import { prisma } from "@/backend/utils/prisma"
+import { PokemonClient } from "pokenode-ts";
+import { prisma } from "@/backend/utils/prisma";
 
 export const t = initTRPC.create();
 
 export const appRouter = t.router({
-    "get-pokemon-by-id": t.procedure
-        .input(
-            z.object({
-                id: z.number()
-            })
-        )
-        .query(async ({ input }) => {
-            const api = new PokemonClient();
-            const pokemon = await api.getPokemonById(input.id);
-            return { name: pokemon.name, sprites: pokemon.sprites };
-        }),
-    "cast-vote": t.procedure
-        .input(
-            z.object({
-                votedFor: z.number(),
-                votedAgainst: z.number()
-            }),
-        ).mutation(async ({ input }) => {
-            const voteInDb = await prisma.vote.create({
-                data: {
-                    ...input
-                }
-            })
-            return { success: true, vote: voteInDb }
-        })
+	"get-pokemon-by-id": t.procedure
+		.input(
+			z.object({
+				id: z.number(),
+			})
+		)
+		.query(async ({ input }) => {
+			const pokemon = await prisma.pokemon.findFirst({
+				where: { id: input.id },
+			});
+			if (!pokemon) throw new Error("Pokemon doesn't exist with this id");
+			return pokemon;
+		}),
+	"cast-vote": t.procedure
+		.input(
+			z.object({
+				votedFor: z.number(),
+				votedAgainst: z.number(),
+			})
+		)
+		.mutation(async ({ input }) => {
+			const voteInDb = await prisma.vote.create({
+				data: {
+					votedAgainstId: input.votedAgainst,
+					votedForId: input.votedFor,
+				},
+			});
+			return { success: true, vote: voteInDb };
+		}),
 });
 
 // .mutation("cast-vote", {
@@ -42,7 +46,6 @@ export const appRouter = t.router({
 //     async resolve() {
 //         return {success: true}
 //     }
-
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
